93dfeecf820d7928a59cbb63626887c922fe2d9c
tomkariath <tomkariath2.0@gmail.com>
2020-10-01 16:08:10 +0530

    Fault tolerance with Histrx

--- limits-service/src/main/java/com/hypocritus/microservice/LimitsServiceConfigurationController.java
+++ limits-service/src/main/java/com/hypocritus/microservice/LimitsServiceConfigurationController.java
@@ -1,10 +1,14 @@
 package com.hypocritus.microservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
+@EnableHystrix
 public class LimitsServiceConfigurationController {
 	
 	@Autowired
@@ -13,5 +17,15 @@ public class LimitsServiceConfigurationController {
 	@GetMapping(path = "/limits")
 	LimitsConfiguration getLimitsConfiguration() {
 		return new LimitsConfiguration(config.getMinimum(), config.getMaximum());
+	}
+	
+	@GetMapping(path = "/faultCheck")
+	@HystrixCommand(fallbackMethod = "fallbackConfiguration")
+	LimitsConfiguration checkFaultTolerance() {
+		throw new RuntimeException("checking fault tolerance");
+	}
+	
+	LimitsConfiguration fallbackConfiguration(){
+		return new LimitsConfiguration(1,23);
 	}
 }
